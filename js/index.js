924800c791b86691bc9f54869e471c65e8542227
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-03-30 10:07:56 -0700

    avances p2c

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 'use strict'
+
+class msg {
+    constructor(pos, cont, time){
+        this.pos = pos
+        this.cont = cont
+        this.time = time
+    }
+
+    node () {
+        let base = document.createElement("div")
+        base.classList.add("msg", this.pos)
+        return base
+    }
+}
+
+const inputTextBox = document.querySelector(".input-text input");
+const sendMsg = document.querySelector(".send-btn").addEventListener("click", ()=>{
+    console.log("clocl")
+    let conversation = document.querySelector(".conversation")
+    conversation.appendChild(new msg("rigth", 1, 2).node())
+})
+
