474eb18132470a5c9953a1e5049e01158e999356
EduardoMoraton <eduardo.moraton@iesdoctorbalmis.com>
2022-03-30 13:07:56 -0700

    avance

--- js/index.js
+++ js/index.js
@@ -9,7 +9,20 @@ class msg {
 
     node () {
         let base = document.createElement("div")
+        let msgBox = document.createElement("div")
+        let content = document.createElement("div")
+        let time = document.createElement("div")
+        let status = document.createElement("div")
         base.classList.add("msg", this.pos)
+        msgBox.classList.add("msg-box")
+        content.classList.add("content")
+        content.textContent = this.cont;
+        time.classList.add("time")
+        msgBox.appendChild(content)
+        msgBox.appendChild(time)
+        msgBox.appendChild(status)
+        base.appendChild(msgBox)
+        console.log(base)
         return base
     }
 }
@@ -18,6 +31,6 @@ const inputTextBox = document.querySelector(".input-text input");
 const sendMsg = document.querySelector(".send-btn").addEventListener("click", ()=>{
     console.log("clocl")
     let conversation = document.querySelector(".conversation")
-    conversation.appendChild(new msg("rigth", 1, 2).node())
+    conversation.appendChild(new msg("left", "hola marcos", 2).node())
 })
 
